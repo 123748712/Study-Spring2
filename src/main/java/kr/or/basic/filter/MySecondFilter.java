0de6d0d3b68d44899ee114c9bf8f3cf294e11e19
123748712 <kemco01@naver.com>
2022-05-04 12:33:56 +0900

    2022.05.04 @WebFilter 어노테이션

--- src/main/java/kr/or/basic/filter/MySecondFilter.java
+++ src/main/java/kr/or/basic/filter/MySecondFilter.java
@@ -12,28 +12,27 @@ import javax.servlet.annotation.WebFilter;
 
 import lombok.extern.slf4j.Slf4j;
 
-// tomcat에서 Spring DispatcherServlet으로 넘어가는 중간에 실행되는게 Filter
 @Slf4j
-@WebFilter(urlPatterns = "/*") // web.xml에 등록하지 않아도 Filter 실행
-public class MyFirstFilter implements Filter {
+@WebFilter(urlPatterns = "/*") // 필터의 순서가 보이지 않아 사용하지 않는다.
+public class MySecondFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		log.info("init");
+		log.info("init 2");
 	}
 	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		log.info("start");
-		request.setAttribute("a", "a"); // filter가 url-pattern에 등록한 url이 실행되면 request에 Attribute로 담아주어 url 실행시 모든곳에서 사용할 수 있다.
+		log.info("start 2");
+		request.setAttribute("a2", "a2"); // filter가 url-pattern에 등록한 url이 실행되면 request에 Attribute로 담아주어 url 실행시 모든곳에서 사용할 수 있다.
 		chain.doFilter(request, response); // Filter Chain의 핵심
-		log.info("end"); // 먼저 호출된 필터 안에 나중에 호출된 필터가 들어가게 되어 나중에 호출된 필터가 종료되어야 먼저 호출된 필터도 종료된다.
+		log.info("end 2");
 
 	}
 	
 	@Override
 	public void destroy() {
-		log.info("destroy");
+		log.info("destroy 2");
 	}
 }
